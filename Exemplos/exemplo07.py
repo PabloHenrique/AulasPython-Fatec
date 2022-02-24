4e0b5c0f8bf73be7a56c5d976d75f17a218cf444
PabloHenrique <pablohenriquenadai@gmail.com>
2022-02-24 04:52:02 -0300

    Update Order. Arquivos

---
+++ Exemplos/exemplo07.py
@@ -0,0 +1,11 @@
+#Exemplo 07 - IF
+
+nome = input("Digite seu nome: ")
+if (nome == 'Pablo'):
+    print("Olá, Pablo!")
+
+elif (nome == 'Henrique'):
+    print("Olá, Henrique!")
+
+else:
+    print(f"Olá, {nome}!")
--- Exercícios/Algoritmo/exe007.py
+++
@@ -1,8 +0,0 @@
-#Ler o valor em reais e mostrar a conversão de seu valor para dólar.
-
-reais = float(input("Digite o valor em reais: "))
-cotacao = float(input("Qual o valor da cotação do Dolar?"))
-
-dolar = reais * cotacao
-
-print(f"O valor R${reais} em Doláres resultam em: U${dolar}")
