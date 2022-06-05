d02fc6d80e416f0c469b363373a2a2233ffed7e7
PabloHenrique <pablohenriquenadai@gmail.com>
2022-06-05 12:00:13 -0300

    Versão Final Trabalho 02

---
+++ Avaliações/trabalho2Versão2.py
@@ -0,0 +1,36 @@
+print("\n\nPablo Henrique Vieira De Nadai")
+print("Trabalho Algoritmo 02 - Insert sort\n\n")
+print('='*20)
+print('Iniciando o programa!')
+
+print("\nDigite os valores desejados")
+print("OBS: Caso digite 0, o programa será encerrado.")
+
+#Vetor utilizado
+M = []
+cont = 0
+j = 0
+#Laço infinito até atender a condição
+#Solicitará números para o vetor
+while True:
+    num = int(input('>>> '))
+    # 0 para encerrar o programa
+    if (num == 0):
+        break
+    else:
+        cont = cont + 1
+        if (len(M) == 0 or num > M[len(M)-1]):
+            M.append(num)
+        for i in range(cont):
+            if (num < M[i]):
+                M.insert(i, num)
+            valor = M[i]
+            #var posição anterior
+            j = i - j
+            while j>=0 and M[i] > valor:
+                M[j+1] = M[j]
+                j -= 1
+
+print("\n\nResultado: ")
+
+print({ *M })
